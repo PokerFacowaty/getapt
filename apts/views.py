098bba018258233b1e931d0bcd0dc2f317952fc7
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-27 18:33:27 +0100

    Fixed views

--- apts/views.py
+++ apts/views.py
@@ -4,5 +4,8 @@ from apts.models import Apartment
 
 def index(request):
     apartments = Apartment.objects.all()
+    for apt in apartments:
+        apt.total = apt.total_cost()
+        apt.costs = apt.COSTS.all()
     context = {"apts": apartments}
     return render(request, "apts/index.html", context)
