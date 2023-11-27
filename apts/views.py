f04c1cd76bef164020ad56c38633dc908d2ea027
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-27 17:59:55 +0100

    Added context to the view

--- apts/views.py
+++ apts/views.py
@@ -5,4 +5,4 @@ from apts.models import Apartment
 def index(request):
     apartments = Apartment.objects.all()
     context = {"apts": apartments}
-    return render(request, "apts/index.html", )
+    return render(request, "apts/index.html", context)
