d33e1a83a8c3f271290729046718bfff9bc26122
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-29 16:17:57 +0100

    Fixed one url and added another

--- apts/urls.py
+++ apts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('add_cost/', views.create_cost)
+    path('add_cost/', views.create_cost),
+    path('add_apt/', views.create_apt)
 ]
