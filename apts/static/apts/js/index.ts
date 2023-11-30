5081d8da0fc424bafeb41d3e87bcc88dfce1ce97
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-11-30 16:51:52 +0100

    Replaced adding a new row with just reloading the site since it's simpler and I don't need the site to not reload

--- apts/static/apts/js/index.ts
+++ apts/static/apts/js/index.ts
@@ -132,12 +132,7 @@ async function addApt(costs_ids: Number[]){
             cost.remove();
         }
         document.getElementById("input").remove();
-        const table = document.getElementsByTagName("table")[0] as HTMLTableElement;
-        const row = table.insertRow();
-        for (const el of [link, sqm, rooms, location, notes]){
-            const cell = row.insertCell();
-            cell.innerHTML = String(el);
-        }
+       window.location.reload();
     });
 }
 
